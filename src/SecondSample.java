c7848a2c1b82ef4495ca19864c229a35ce908bfc
Jaideep <jaideep@192.168.58.1>
2021-05-17 10:22:57 +0530

    updated the secondsample.java class

--- src/SecondSample.java
+++ src/SecondSample.java
@@ -4,6 +4,7 @@ public class SecondSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is for second sample");
+		System.out.println("jaideep");
 
 	}
 
